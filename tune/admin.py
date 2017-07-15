ff1551a689350d410dc4f2eaaef868f632b6f00f
Aarshiya <aarshiyaguneja@gmail.com>
2017-07-15 19:48:41 +0530

    added objects to database

--- tune/admin.py
+++ tune/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from tune.models import Songs
+class SongsAdmin(admin.ModelAdmin):
+    model = Songs
+    list_display = ('name', 'description',)
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Songs, SongsAdmin)
+
