d83ea70a245273e67b3520278bda74a56f038cf1
Aarshiya <aarshiyaguneja@gmail.com>
2017-07-19 21:21:23 +0530

    dynamic templates

--- tune/views.py
+++ tune/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
 
-def index(request): 
-    return render(request, 'index.html')
+from django.shortcuts import render
+from tune.models import Songs
+
+
+def index(request):
+    songs = Songs.objects.all()
+
+    return render(request, 'index.html', {
+        'songs': songs,
+    })
+
